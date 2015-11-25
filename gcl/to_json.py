38746e4f4891f7ad87ce678776be15556d1db449
Rico Huijbers <rix0rrr@gmail.com>
2015-11-25 19:54:12 +0100

    Add proper root selector to gcl2json

--- gcl/to_json.py
+++ gcl/to_json.py
@@ -7,12 +7,24 @@ from gcl import query
 from gcl import util
 
 
+def select(dct, path):
+  for part in path:
+    if not hasattr(dct, 'keys'):
+      raise RuntimeError('Value %r cannot be indexed with %r' % (dct, part))
+    if part not in dct:
+      raise RuntimeError('Value %r has no key %r' % (dct, part))
+    dct = dct[part]
+  return dct
+
+
 def main(argv=None, stdin=None):
   parser = argparse.ArgumentParser(description='Convert (parts of) a GCL model file to JSON.')
   parser.add_argument('file', metavar='FILE', type=str, nargs='?',
                       help='File to parse')
   parser.add_argument('selectors', metavar='SELECTOR', type=str, nargs='*',
-                      help='Subnodes to convert. The first selector will be treated as the root of the printed output.')
+                      help='Select nodes to include in the JSON.')
+  parser.add_argument('--root', '-r', metavar='PATH', type=str, default='',
+                      help='Use the indicated root path as the root of the output JSON object (like a.b.c but without wildcards)')
 
   args = parser.parse_args(argv or sys.argv[1:])
 
@@ -27,7 +39,11 @@ def main(argv=None, stdin=None):
       model = sels.select(model).deep()
 
     plain = util.to_python(model)
-    sys.stdout.write(json.dumps(plain))
+
+    selectors = args.root.split('.') if args.root else []
+    selected = select(plain, selectors)
+
+    sys.stdout.write(json.dumps(selected))
   except (gcl.ParseError, RuntimeError) as e:
     sys.stderr.write(str(e) + '\n')
     sys.exit(1)
