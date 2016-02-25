0ebb30b2d9147784e8b1a5d2fb1651a6aac02f30
Rico Huijbers <rix0rrr@gmail.com>
2016-02-25 21:36:36 +0100

    Bump version and Python 3 compat

--- gcl/__init__.py
+++ gcl/__init__.py
@@ -15,7 +15,7 @@ from . import util
 from . import framework
 
 
-__version__ = '0.6.5'
+__version__ = '0.6.6'
 
 
 # Namespace copy for backwards compatibility.
--- gcl/query.py
+++ gcl/query.py
@@ -56,7 +56,7 @@ def partition(pred, iterable):
     'Use a predicate to partition entries into false entries and true entries'
     # partition(is_odd, range(10)) --> 0 2 4 6 8   and  1 3 5 7 9
     t1, t2 = itertools.tee(iterable)
-    return filter(negate(pred), t1), filter(pred, t2)
+    return list(filter(negate(pred), t1)), list(filter(pred, t2))
 
 
 class GPath(object):
